a1caa0d5568984aab95b32d8728afaabdf7a520c
Martha659 <martha.blackcloud@students.msswarriors.org>
2025-11-21 19:46:17 +0000

    77

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function guessNumber(guess) {
-  if (guess == 25) {
+  if (guess != 25) {
     return; 
   }
 return "not it";
 }
 
 function strictGuessNumber(guess) {
-  if (typeof guess === "number" && guess !== 25) {
+  if ( guess !== 25) {
     return "not it";
   }
 }
